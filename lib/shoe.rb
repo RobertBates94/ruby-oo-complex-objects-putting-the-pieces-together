58e063dd3d406e90dc8026fa67d140a640f7c78d
RobertBates94 <bates.to.infinity@gmail.com>
2021-03-11 17:38:35 -0600

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoes
+    attr_accessor :color, :size, :material, :condition, :repaired
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand
+        @brand
+    end
+
+end
